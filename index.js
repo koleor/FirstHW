c908a809bcb6537fc95c45125c86d54f25b100d6
koleor <egor_k999@mail.ru>
2025-02-23 10:00:47 +0400

    upd

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// подключаем модуль http
 const express = require('./node_modules/express');
 const app = express(); 
 const port = 3000;
 
-// создаем сервер
 app.get ('/', (req, res) =>{
 
     res.send('Привет, мир :D');
 });
 
-// слушаем порт 3000
 app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:3000`);
   });
