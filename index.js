b240cb9e70807135dae7ac19926078c0e836250a
koleor <egor_k999@mail.ru>
2025-02-23 09:55:35 +0400

    Hello world upd

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 // подключаем модуль http
-const http = require('http');
+const express = require('./node_modules/express');
+const app = express(); 
+const port = 3000;
 
 // создаем сервер
-const server = http.createServer((req, res) => {
+app.get ('/', (req, res) =>{
 
-    // устанавливаем заголовок Content-Type
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // отправляем ответ "Hello World"
-    res.end('Hello World');
-
+    res.send('Привет, мир :D');
 });
 
 // слушаем порт 3000
-server.listen(3000, '192.168.0.1');
-
-console.log('Server running at http://192.168.0.1:3000/');
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:3000`);
+  });
